84ef3845f2246238d95b4dd7b7facef7c443a704
sunzehai <zehai.sun@icloud.com>
2021-02-03 23:49:08 +0900

    初版作成中

--- src/main/database/table/article.sql
+++ src/main/database/table/article.sql
@@ -1,10 +1,11 @@
 CREATE TABLE article (
     id                  INT                 NOT NULL
   , authorId            INT                 NOT NULL
-  , categoryId          INT                 NOT NULL
+  , parentId            INT                 NOT NULL
   , title               VARCHAR(75)         NOT NULL
   , summary             TINYTEXT            NOT NULL
   , published           CHAR(1)             NOT NULL
-  , createDate          DATE                NOT NULL
+  , createdAt           DATETIME            NOT NULL
+  , updatedAt           DATETIME            NULL
   , PRIMARY KEY (id)
 )
